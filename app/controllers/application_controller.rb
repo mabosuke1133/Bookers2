b9dfa23638217a38b76885f462e189dbbbd0ee70
Ubuntu <245080863+mabosuke1133@>
2026-01-08 01:05:57 +0000

    ログイン、ログアウト後の遷移先設定完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # ログイン後の遷移先設定
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  # ログアウト後の遷移先設定
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
   protected
   
