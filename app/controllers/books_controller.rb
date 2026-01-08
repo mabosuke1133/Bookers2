bd7cb06bb5efd8af964ea6b3f3a169f9c3296142
Ubuntu <245080863+mabosuke1133@>
2026-01-08 05:24:43 +0000

    bookのindexページの右側作成完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
   def index
+   @book = Book.new      # 新規投稿用の空のモデル（New book用）
+   @books = Book.all     # 全ての投稿を取得
+   @user = current_user   # ログイン中のユーザー（User info用）
   end
 
   def show
