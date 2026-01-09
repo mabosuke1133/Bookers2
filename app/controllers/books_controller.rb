78af66b45ae8fcadd2cc3c874e1bde026246bb71
Ubuntu <245080863+mabosuke1133@>
2026-01-09 13:45:36 +0000

    bookのupdate機能の作成完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,7 @@ class BooksController < ApplicationController
 
   def update
    @book = Book.find(params[:id])
+   
    if @book.update(book_params) # 編集内容を保存
     flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book.id) # 詳細ページへ戻る
