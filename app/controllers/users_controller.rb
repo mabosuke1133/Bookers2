492042fd0a8e414eb433de5aeb26720e93264582
Ubuntu <245080863+mabosuke1133@>
2026-01-10 13:05:31 +0000

    userのshowページ作成完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,9 @@ class UsersController < ApplicationController
   end
 
   def show
+  @user = User.find(params[:id])    # URLのIDから「誰のページか」を特定する
+  @books = @user.books      # そのユーザー（@user）に紐付いた本だけをすべて取得する
+  @book = Book.new       # サイドバーの「New book」用
   end
 
   def edit
