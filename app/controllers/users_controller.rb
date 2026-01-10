2a401222da4ba525eaeb8269fab45023162c1fc1
Ubuntu <245080863+mabosuke1133@>
2026-01-10 12:55:05 +0000

    userのindexページ作成完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
+   @users = User.all       # 全ユーザーを取得して一覧に出す
+   @book = Book.new        # サイドバーの「New book」用
+   @user = current_user    # サイドバーの「User info」用
   end
 
   def show
