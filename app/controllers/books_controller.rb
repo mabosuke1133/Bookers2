e91f865877d7a6b221d609e41dfbec20a43dda5e
Ubuntu <245080863+mabosuke1133@>
2026-01-11 05:15:36 +0000

    アクセス制限の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,8 @@
 class BooksController < ApplicationController
   # ログインしていない場合はログイン画面に送る
   before_action :authenticate_user!
+  # editアクションの前に「本人確認」を実行する
+  before_action :is_matching_login_user, only: [:edit, :update]
   
   def index
    @book = Book.new
@@ -29,35 +31,45 @@ class BooksController < ApplicationController
   end
 
   def create
-   # 1. フォームから送られてきたデータ(title, body)を箱に入れる
+   # フォームから送られてきたデータ(title, body)を箱に入れる
    @book = Book.new(book_params)
     
-    # 2. 【重要】「誰の投稿か」を特定するため、ログインユーザーのIDを代入
+   #【重要】「誰の投稿か」を特定するため、ログインユーザーのIDを代入
    @book.user_id = current_user.id
     
-   # 3. データベースに保存（セーブ）を実行
+   # データベースに保存（セーブ）を実行
    if @book.save
-     # 4. 【showへのリンク】保存成功したら「本の詳細ページ」へ飛ばす
+     #【showへのリンク】保存成功したら「本の詳細ページ」へ飛ばす
      flash[:notice] = "You have created book successfully."
      redirect_to book_path(@book.id)
    else
-     # 5. 保存失敗（タイトルが空など）した時の処理
+     # 保存失敗（タイトルが空など）した時の処理
      @books = Book.all
      @user = current_user
      render :index # 一覧ページを再表示
    end
-end
+  end
 
-def destroy
+ def destroy
   book = Book.find(params[:id])
   book.destroy
   redirect_to books_path
-end
+ end
 
-private
+ private
 
-  # 6. セキュリティのための許可リスト
-  def book_params
-    params.require(:book).permit(:title, :body)
+ # セキュリティのための許可リスト
+ def book_params
+   params.require(:book).permit(:title, :body)
+ end
+
+ # 本の投稿者が自分かどうかをチェックするメソッド
+ def is_matching_login_user
+  @book = Book.find(params[:id])
+   # その本のユーザーIDと、今ログインしているユーザーのIDが一致しなければ
+  unless @book.user.id == current_user.id
+     # 本の一覧画面に強制送還！
+     redirect_to books_path
   end
+ end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   # ログインしていない場合はログイン画面に送る
   before_action :authenticate_user!
+  # 編集(edit)と更新(update)の前に、本人確認メソッドを動かす
+  before_action :ensure_correct_user, only: [:edit, :update]
   
   def index
    @users = User.all       # 全ユーザーを取得して一覧に出す
@@ -9,20 +11,20 @@ class UsersController < ApplicationController
   end
 
   def show
-  @user = User.find(params[:id])    # URLのIDから「誰のページか」を特定する
-  @books = @user.books      # そのユーザー（@user）に紐付いた本だけをすべて取得する
-  @book = Book.new       # サイドバーの「New book」用
+   @user = User.find(params[:id])    # URLのIDから「誰のページか」を特定する
+   @books = @user.books      # そのユーザー（@user）に紐付いた本だけをすべて取得する
+   @book = Book.new       # サイドバーの「New book」用
   end
 
   def edit
-  @user = User.find(params[:id])
+   @user = User.find(params[:id]) # ここで改めてチェックしなくても、before_actionが守ってくれる
   end
 
   def update
-  @user = User.find(params[:id])
-  if @user.update(user_params)
-    redirect_to user_path(@user.id), notice: "You have updated user successfully."
-  else
+   @user = User.find(params[:id])
+   if @user.update(user_params)
+     redirect_to user_path(@user.id), notice: "You have updated user successfully."
+   else
     render :edit
   end
 end
@@ -31,4 +33,13 @@ private
 
 def user_params
   params.require(:user).permit(:name, :introduction, :profile_image)
+end
+
+def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      # もし本人じゃなければ、自分の詳細ページ(show)に強制送還！
+      redirect_to user_path(current_user)
+    end
+  end
 end
