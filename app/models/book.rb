550fd16fdfd5a35932e7073f7ccbf397bf502fa9
Ubuntu <245080863+mabosuke1133@>
2026-01-11 04:28:24 +0000

    新規投稿が失敗したときのバリデーション実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
+
+  # Titleは空ではない（必須）
+  validates :title, presence: true
+  # Opinion（body）は空ではなく、かつ200文字以内
+  validates :body, presence: true, length: { maximum: 200 }
 end
